5a68e4681aba992008b9e623bc4f1a55c45c73a2
Camilo Zambrano <camilozambranovotto@hotmail.com>
2021-10-11 20:32:57 -0500

    Innitial script created :tada:
    This is the basic script. So far it works by calling it from the console

---
+++ script.js
@@ -0,0 +1,23 @@
+function autoFarm() {
+	"use strict";
+	const intervalId = setInterval(() => {
+		try {
+			const energyElem = document.querySelector("tw-expression[name=energy]");
+			const maxEnergyElem = document.querySelector("tw-expression[name=maxEnergy]");
+			if (energyElem.textContent !== maxEnergyElem.textContent) {
+				document.getElementsByTagName("tw-enchantment")[0].click();
+				console.log("clicked the 'fight' button");
+			} else {
+				console.log("energy is set, we don't need to farm anymore!");
+				clearInterval(intervalId );
+			}
+		}
+		catch(e) {
+			console.log("uh oh! we found an issue:");
+			console.error(e);
+			console.log("stopping the interval...");
+			clearInterval(intervalId );
+		}
+	}, 400);
+	console.log("starting to farm...")
+}
